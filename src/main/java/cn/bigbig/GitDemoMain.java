28097afcf3819c3260101f7be9ff58945ca9e693
cy15305668755 <329728547@qq.com>
2018-02-22 15:49:52 +0800

    My first git demo

---
+++ src/main/java/cn/bigbig/GitDemoMain.java
@@ -0,0 +1,12 @@
+package cn.bigbig;
+
+/**
+ * Created by jiangyi on 2018/2/22.
+ */
+public class GitDemoMain {
+
+    public static void main(String[] args) {
+        System.out.println("This my first Git Demo...");
+    }
+
+}
